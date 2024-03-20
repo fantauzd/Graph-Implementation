ae49ce15aed2ed666959c98175091fe8ee7a0ac2
fantauzd <fantauzd@oregonstate.edu>
2024-03-20 01:08:41 -0400

    Add Dijkstra psuedocade

--- Graph.py
+++ Graph.py
@@ -1,5 +1,6 @@
 """
 Depth-first search the shortest distance algorithm:
+
 To find the set of vertices reachable from an initial vertex vs
 Create and Initialize set of reachable vertices
 Add vs to a stack
@@ -10,3 +11,20 @@ While stack is not empty
         For all neighbors, vj, of v
         If vj is not is set of reachable vertices, add to stack
 """
+
+
+"""
+Dijkstra’s Shortest Path Algorithm (Weighted Breadth-first:
+
+To find the shortest distance to vertices reachable from an initial vertex vs
+Initialize dictionary of reachable vertices with vs having cost zero,
+and add vs to a priority queue with distance zero
+While priority queue is not empty
+    Get and remove (pop) vertex v with shortest distance from priority queue
+    If v is known to be reachable, discard
+    Otherwise, add v with given cost to dictionary of reachable vertices
+        For all neighbors, vj, of v
+        If vj is not is set of reachable vertices, combine cost of reaching v
+        with cost to travel from v to vj, and add to priority queue 
+"""
+
